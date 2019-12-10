d43ec75454997ccbd88ac45de43f6590ebe4aea7
Omar Gaye <omar.gaye@ibm.com>
2019-12-10 10:01:34 -0600

    adding vcap feature

--- src/main/java/com/ibm/cpat/poc/HelloController.java
+++ src/main/java/com/ibm/cpat/poc/HelloController.java
@@ -2,6 +2,7 @@ package com.ibm.cpat.poc;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,11 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class HelloController {
 
+    @Value("${demo.name}")
+    private String name;
+
     private String greeting;
-    @GetMapping(path = "hello")
-    public String sayHello(@RequestParam(value = "name", required = false) String name) {
-        greeting = StringUtils.isEmpty(name)? "Hello stranger!!!" : String.format("Hello %s", name);
+
+    @GetMapping(path = "/hello")
+    public String sayHello() {
+
+        greeting = String.format("Hello %s", name);
         log.info("************** {}", greeting);
+
         return greeting;
     }
 }
